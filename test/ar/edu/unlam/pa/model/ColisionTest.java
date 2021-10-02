a6df552c7f6aae15a0e2123ecf6dc90b4cb9a16d
rrondosa <38527755+rrondosa@users.noreply.github.com>
2021-10-01 21:43:15 -0300

    ej de commit

--- test/ar/edu/unlam/pa/model/ColisionTest.java
+++ test/ar/edu/unlam/pa/model/ColisionTest.java
@@ -7,7 +7,7 @@ import org.junit.After;
 import org.junit.Test;
 
 public class ColisionTest {
-//	Serian cualquiel personaje del juego 
+	
 	AvionPlayer player1;
 	AvionPlayer player2;
 	AvionVerdeChiquito enemigo1;
